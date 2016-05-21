f3cd2aa35fd2b58a130a6803729f801d965e77a6
Gregory Kan <kan.gregory@gmail.com>
2016-05-21 16:59:01 +1200

    added new service that generates markov chain

---
+++ app/services/gardens/walk.rb
@@ -0,0 +1,24 @@
+class Gardens::Walk
+
+  def initialize(garden)
+    @garden = garden
+    @authors = garden.authors
+  end
+
+  def walk
+    markov_chain_generator.get_sentences(number_of_sentences).compact.join
+  end
+
+  def combined_authors_works
+    Work.where(author: @authors).map(&:text).join(". ")
+  end
+
+  def number_of_sentences
+    combined_authors_works.split(".").length
+  end
+
+  def markov_chain_generator
+    MarkovChains::Generator.new(combined_authors_works)
+  end
+
+end
