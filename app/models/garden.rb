51e6a0113517e9b505d1d2dabbe1df9bcc825396
Gregory Kan <kan.gregory@gmail.com>
2016-05-21 16:33:28 +1200

    added computed property for rendering authors' names

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -3,4 +3,8 @@ class Garden < ActiveRecord::Base
   has_many :garden_authors
   has_many :authors, through: :garden_authors
 
+  def author_names
+    authors.map { |x| x.name }.join(", ")
+  end
+
 end
